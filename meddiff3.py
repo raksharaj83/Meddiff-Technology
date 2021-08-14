4db8af3e7eac534e15900d112692210d2be8f787
RAKSHA RAJ <44027419+raksharaj83@users.noreply.github.com>
2021-08-14 11:26:13 +0530

    Update meddiff3.py

--- meddiff3.py
+++ meddiff3.py
@@ -1,3 +1,21 @@
+'''Write a function that provides a change directory (cd) function for an abstract file system.
+
+Notes:
+Root path is '/'.
+Path separator is '/'.
+Parent directory is addressable as '..'.
+Directory names consist only of English alphabet letters (A-Z and a-z).
+The function should support both relative and absolute paths.
+The function will not be passed any invalid paths.
+Do not use built-in path-related functions.
+
+For example:
+path = Path('/a/b/c/d')
+path.cd('../x')
+print(path.current_path)
+
+Output:
+Should display '/a/b/c/x'.'''
 class Path:
     def __init__(self, path):
         self.current_path = path
