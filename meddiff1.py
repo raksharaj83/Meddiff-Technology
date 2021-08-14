396e8928da0aeb000978f8d072e605c619d07df8
RAKSHA RAJ <44027419+raksharaj83@users.noreply.github.com>
2021-08-14 11:23:48 +0530

    Update meddiff1.py

--- meddiff1.py
+++ meddiff1.py
@@ -1,7 +1,16 @@
+'''Implement a group_by_owners function that:
+
+·         Accepts a dictionary containing the file owner name for each file name.
+
+·         Returns a dictionary containing a list of file names for each owner name, in any order.
+
+For example, for dictionary {'Input.txt': 'Randy', 'Code.py': 'Stan', 'Output.txt': 'Randy'} the group_by_owners function should return {'Randy': ['Input.txt', 'Output.txt'], 'Stan': ['Code.py']}.
+
+ '''
 def group_by_owners(files):
     result = {}
-    for file, owner in files.items():  # use files.iteritems() on Python 2.x
-        result[owner] = result.get(owner, []) + [file]  # you can use setdefault(), too
+    for file, owner in files.items():  
+        result[owner] = result.get(owner, []) + [file]  
     return result
 
 files = {
